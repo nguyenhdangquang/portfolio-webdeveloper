585ea513e040774c448dcba45bdb88a030fbe72e
quang.nguyenhd <nguyenhdangquang@gmail.com>
2024-10-25 15:03:40 +0700

    feat: update package

--- components/Cursor/index.js
+++ components/Cursor/index.js
@@ -1,38 +1,41 @@
-import React, { useEffect, useState } from "react";
-import CustomCursor from "custom-cursor-react";
-import "custom-cursor-react/dist/index.css";
-import { useTheme } from "next-themes";
+import React, { useMemo } from "react";
+import AnimatedCursor from "react-animated-cursor"
+import { useCustomThemeMounted } from "../../hook/useCustomThemeMounted";
 
 const Cursor = () => {
-  const theme = useTheme();
-  const [mount, setMount] = useState();
+  const { theme, mounted} = useCustomThemeMounted();
 
-  const getCusomColor = () => {
-    if (theme.theme === "dark") {
-      return "#ffff00";
-    } else if (theme.theme === "light") {
-      return "#0063a6";
+  const getCustomColor = useMemo(() => {
+    if (theme === "dark") {
+      return '255, 255, 0';
+    } else if (theme === "light") {
+      return "0,99,166";
     }
-  };
+  }, [theme]);
 
-  useEffect(() => {
-    setMount(true);
-  }, []);
   return (
     <>
-      {mount && (
-        <CustomCursor
-          targets={[".link"]}
-          customClass="custom-cursor"
-          dimensions={30}
-          fill={getCusomColor()}
-          smoothness={{
-            movement: 0.2,
-            scale: 0.1,
-            opacity: 0.2,
-          }}
-          targetOpacity={0.5}
-          targetScale={2}
+      {mounted && (
+        <AnimatedCursor
+          innerSize={15}
+          outerSize={5}
+          color={getCustomColor}
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={5}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link'
+          ]}
         />
       )}
     </>
